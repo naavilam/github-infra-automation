c02a4af4a664a4c397785083d8429793d790ee37
Nara Avila <nara.moraes@usp.br>
2025-12-29 15:28:22 -0300

    Update files

---
+++ modules/github-dispatcher-app/variables.tf
@@ -0,0 +1,18 @@
+variable "cors_origin" { type = string }
+
+variable "env"         { type = string }
+variable "name_prefix" { type = string }
+variable "component"   { type = string }
+
+variable "github_app_id"          { type = string }
+variable "github_installation_id" { type = string }
+
+variable "github_private_key_pem" {
+  type      = string
+  sensitive = true
+}
+
+variable "dispatch_shared_secret" {
+  type      = string
+  sensitive = true
+}
