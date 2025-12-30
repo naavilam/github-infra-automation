d74464d279275b3e86438254570c340b2482db11
Nara Avila <nara.moraes@usp.br>
2025-12-30 00:48:44 -0300

    Update files

--- modules/github-dispatcher-app/codebuild.tf
+++ modules/github-dispatcher-app/codebuild.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_public_access_block" "lambda_artifacts" {
 
 
 resource "aws_codebuild_project" "lambda_builder" {
-  name         = "${var.component}-lambda-builder"
+  name         = "lambda-builder"
   service_role = aws_iam_role.codebuild.arn
 
   artifacts { type = "NO_ARTIFACTS" }
