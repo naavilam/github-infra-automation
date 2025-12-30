72e7093ce22fb66811be1d184b37359fb64d5a6e
Nara Avila <nara.moraes@usp.br>
2025-12-30 00:06:58 -0300

    Update files

--- workspaces/academic-codex/main.tf
+++ workspaces/academic-codex/main.tf
@@ -41,7 +41,6 @@ module "app" {
 
 variable "cors_origin" { type = string }
 
-
 variable "env"         { type = string }
 variable "name_prefix" { type = string }
 variable "component"   { type = string }
