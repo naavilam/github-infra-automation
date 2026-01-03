904a0f9c00f58ece7771b7f8b6e1a09e6dd5c386
Nara Avila <nara.moraes@usp.br>
2026-01-03 18:03:14 -0300

    Update files

--- modules/github-bootstrap/role.tf
+++ modules/github-bootstrap/role.tf
@@ -34,7 +34,8 @@ data "aws_iam_policy_document" "assume_github" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        "repo:naavilam/hub:*"
+        "repo:${var.github_owner}/${var.github_repo}:ref:${var.github_ref}",
+        "repo:${var.github_owner}/${var.github_repo}:environment:*",
       ]
     }
   }
