360c4e657cc14ac277fbd1f29b13190482c2071e
Nara Avila <nara.moraes@usp.br>
2026-06-21 10:34:56 -0300

    Update files

--- modules/github-bootstrap/output.tf
+++ modules/github-bootstrap/output.tf
@@ -1,7 +1,3 @@
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
-
-output "artifact_bucket" {
-  value = aws_s3_bucket.lambda_artifacts.bucket
-}
--- modules/github-bootstrap/role.tf
+++ modules/github-bootstrap/role.tf
@@ -14,10 +14,10 @@ data "aws_iam_policy_document" "assume_github" {
       values   = ["sts.amazonaws.com"]
     }
 
-    # restringe a origem (repo + ref)
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
+
       values = [
         "repo:${var.github_owner}/${var.github_repo}:ref:${var.github_ref}",
         "repo:${var.github_owner}/${var.github_repo}:environment:*"
@@ -29,4 +29,27 @@ data "aws_iam_policy_document" "assume_github" {
 resource "aws_iam_role" "github_actions" {
   name               = "github-actions-deployer"
   assume_role_policy = data.aws_iam_policy_document.assume_github.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "github_actions" {
+
+  statement {
+    actions = [
+      "lambda:UpdateFunctionCode",
+      "lambda:GetFunction",
+      "lambda:GetFunctionConfiguration"
+    ]
+
+    resources = [
+      "arn:aws:lambda:us-east-2:${data.aws_caller_identity.current.account_id}:function:*-dispatcher"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name   = "github-actions-deployer-policy"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.github_actions.json
 }
